b5a946bc750d2deed43902c90208d162b80b051a
Yarden zamir <make.charcoal@gmail.com>
2022-12-11 18:07:55 +0200

    Create gh.functions.zsh

---
+++ Scripts/zshenv/gh.functions.zsh
@@ -0,0 +1,33 @@
+function gh_remove_runner(){
+    repo=${1:-$MY_SERVICE}
+    id=${2:-$GITHUB_RUNNER_ID}
+    # GitHub CLI api
+    # https://cli.github.com/manual/gh_api
+
+    gh api \
+    --method DELETE \
+    -H "Accept: application/vnd.github+json" \
+    /repos/$repo/actions/runners/$id
+}
+
+function gh_add_runner(){
+    name=${1:-$GITHUB_RUNNER_NAME}
+    echo $name
+    labels=${5:-""}
+    repo="https://github.com/"${2:-$MY_SERVICE}
+    github_token=${3:-$GITHUB_TOKEN}
+    runner_group=${4:-"default"}
+
+
+    docker run -d --restart always --name $name \
+        -e REPO_URL=$repo\
+        -e RUNNER_NAME=$name \
+        -e ACCESS_TOKEN=$github_token \
+        -e RUNNER_WORKDIR="/tmp/$name-$(basename $repo)" \
+        -e RUNNER_GROUP=$runner_group \
+        -e LABELS=$labels \
+        -v /var/run/docker.sock:/var/run/docker.sock \
+        -v /tmp/github-runner-your-repo:/tmp/github-runner-data-app-design-systest \
+        myoung34/github-runner:latest
+}
+# gh_add_runner qlik-trial/data-app-design-systest kcw-0 $GH_RUNNERGEN_TOKEN default potato,tomato,chair
