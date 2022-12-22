d5fabcf61b29138a63991dde4eba9494c4d214a4
Yarden zamir <dev@yarden-zamir.com>
2022-12-22 23:53:19 +0200

    Remove redundant stuff

--- Scripts/zshenv/aliases.zsh
+++ Scripts/zshenv/aliases.zsh
@@ -1,8 +1,3 @@
-# alias -s sh=code
-
-alias python=python3
-alias py=python3
-alias pip=pip3
 
 alias kube=kubectl
 alias snow-pass-change="python ~/GitHub/Snowflake-password-helper./change_password_to.py"
@@ -10,30 +5,25 @@ alias snow-pass-request="python ~/GitHub/Snowflake-password-helper./request_pass
 
 alias m=make
 
-alias dad="cd ~/GitHub/data-app-design"
-alias zshenv="cd ~/Scripts/zshenv"
-
 alias dotform="dotnet format"
 
 alias t=touch
 alias create=touch
-
-alias filter="gum filter"
 
 alias secrets="cat ~/Scripts/zshenv/env.secrets.zsh | fzf | sed 's/.*=//'"
 
 alias copy="pbcopy"
 alias paste="pbpaste"
 
-alias functions-folder="~/Scripts/zshenv/functions.zsh"
+alias edit="code"
+alias edit.="code ."
+alias edit-here="code ."
 
-alias edit="code ."
 alias cd..="cd .."
 alias cd...="cd ../.."
 alias cd....="cd ../../.."
 alias cd.....="cd ../../../.."
 alias cd......="cd ../../../../.."
 
-alias open-here="open ."
 alias open.="open ."
-# alias mkdir="mkdir --parents"
+alias open-here="open ."
