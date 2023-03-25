95250baf43c93f6a7b7e4710b93bc09d4fb3c811
Yarden zamir <make.charcoal@gmail.com>
2023-03-25 21:02:01 +0300

    Create aliases.zsh

---
+++ zshrc/aliases.zsh
@@ -0,0 +1,42 @@
+alias copy="pbcopy"
+alias paste="pbpaste"
+
+alias dotform="dotnet format"
+
+alias t=touch
+alias create=touch
+
+alias code.="code ."
+
+alias edit="code"
+alias edit.="code ."
+
+alias cd.="cd $PWD"
+alias cd..="cd .."
+alias cd...="cd ../.."
+alias cd....="cd ../../.."
+alias cd.....="cd ../../../.."
+alias cd......="cd ../../../../.."
+
+alias cd/="cd /"
+alias cd~="cd ~"
+alias cd-="cd -"
+
+alias open.="open ."
+
+alias y=yay
+
+alias @="xargs -L1 -P0"
+alias @L1="xargs -L1 -P0"
+alias @n1="xargs -n1 -P0"
+
+
+alias for-each="xargs -L1 -P0"
+
+alias count='wc -l'
+alias ll='ls -l'
+
+alias set_shell="chsh -s"
+
+alias root='cd $(git rev-parse --show-toplevel || echo ".")'
+alias .=root
