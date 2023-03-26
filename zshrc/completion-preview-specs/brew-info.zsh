de4f55d68b04a3dd10e27e33f691dccf7ec6ff7a
Yarden zamir <make.charcoal@gmail.com>
2023-03-26 22:26:13 +0300

    Prettier brew preview

--- zshrc/completion-preview-specs/brew-info.zsh
+++ zshrc/completion-preview-specs/brew-info.zsh
@@ -1,5 +1,7 @@
 # :fzf-tab:complete:brew-(install|uninstall|reinstall|upgrade|pin|unpin|link|unlink|info|cat|uses|dep):*
 
 brew desc --eval-all $word | cut -d: -f2\
-&& echo "\n# brew info:" && brew info $word | bat --color=always --style=plain --language=markdn \
-&& echo "\n# brew log:" && brew log --stat --max-count 2 $word | bat --color=always --style=plain --language=markdn
+&& echo "\n# brew info:" | bat --color=always --style=plain --language=markdn \
+ && brew info $word | bat --color=always --style=plain --language=markdn \
+&& echo "\n# brew log:" | bat --color=always --style=plain --language=markdn \
+ && brew log --stat --max-count 2 $word | bat --color=always --style=plain --language=markdn
