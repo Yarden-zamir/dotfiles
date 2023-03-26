42898f0096c53bff4e68dcb62b12563ebedbef90
Yarden zamir <make.charcoal@gmail.com>
2023-03-26 22:25:35 +0300

    Move tldr to 2nd priority

--- zshrc/completion-preview-specs/-command-.zsh
+++ zshrc/completion-preview-specs/-command-.zsh
@@ -3,8 +3,8 @@
 export COLUMNS=$(($FZF_PREVIEW_COLUMNS - 2))
 case $group in
 '[external command]'|'[executable file]'|'[builtin command]'|['builtin command'])
-  (out=$(tldr "$word") 2>/dev/null && echo $out) ||\
   (out=$(man $word) 2>/dev/null && echo $out) ||\
+  (out=$(tldr "$word") 2>/dev/null && echo $out) ||\
   (out=$($word --help) 2>/dev/null && echo $out) ||\
   (out=$(which "$word") && echo $out) ||\
   echo "$word"
