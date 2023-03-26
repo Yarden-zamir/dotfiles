617412da7932ef64a902a8d9bedae6fa1e0f79b0
Yarden zamir <make.charcoal@gmail.com>
2023-03-26 15:44:41 +0300

    Rename folder

--- zshrc/pre-init/compinit.zsh
+++ zshrc/pre-init/compinit.zsh
@@ -1,4 +1,4 @@
-FPATH=/Users/kcw/.zfunc/:$FPATH
+FPATH=$HOME/.zfunc/:$FPATH
 FPATH=/usr/share/zsh/5.8.1/functions:$FPATH
 FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
 autoload -Uz compinit
