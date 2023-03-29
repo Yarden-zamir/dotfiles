8794aa82141e016795e8b5d719892f6fbcdc91de
Yarden zamir <make.charcoal@gmail.com>
2023-03-29 16:07:57 +0300

    Update wrap.zsh

--- zshrc/init/wrap.zsh
+++ zshrc/init/wrap.zsh
@@ -2,4 +2,6 @@
 export PATH="$HOME/.wrap/output:$PATH"
 (wrap gen \
     --input-path "$HOME/GitHub/wrap/app/examples:\
-                  $DOTFILES/wrap/scripts"&) # uses defaults, run wrap gen --help for more info
+                  $DOTFILES/wrap/scripts"\
+    --no-clean-output-path \
+&) # uses defaults, run wrap gen --help for more info
