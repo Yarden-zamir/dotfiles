2fd864fb065c87cb6c1cde7c9e3a0cebcd026261
Yarden zamir <make.charcoal@gmail.com>
2023-04-03 20:25:28 +0300

    Replace preview specs in dotfiles to ones from tab-source

--- zshrc/completion-preview-specs/gh.zsh
+++
@@ -1,4 +0,0 @@
-# :fzf-tab:complete:gh:
-query="$words$word"
-query=${${query%%--*}%% } # remove options and trailing space
-echo $query --help | bat --color=always -plhelp
--- zshrc/completion-preview-specs/man.zsh
+++
@@ -1,2 +0,0 @@
-# :fzf-tab:complete:(\\|*/|)man:
-run-help $word > /dev/null 2>&1 && run-help $word | bat --color=always -pl Manpage || $word --help
