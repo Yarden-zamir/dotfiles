93f574aea84ff120cb1c617d78594c2d529d0f44
Yarden zamir <make.charcoal@gmail.com>
2023-04-03 20:24:19 +0300

    Add some folders to navigation

--- zshrc/init/navigation.zsh
+++ zshrc/init/navigation.zsh
@@ -1,18 +1,22 @@
 INDEX_FOLDERS=(~/Github ~/Desktop )
 
-all_items=""
+all_items="$HOME/Desktop
+/opt/homebrew
+$HOME/Downloads
+"
 for folder in $INDEX_FOLDERS; do
     all_items+="$folder\n"
     all_items+="$(find $folder -type d -maxdepth 1 -mindepth 1 -printf "%p\n")\n"
 done
 
+
 navigate(){
-    cd -- "$(echo -e $all_items | eval "fzf $FZF_CTRL_T_OPTS --with-nth=-1 --delimiter='/'")"
+    cd -- "$(echo -e $all_items | eval "fzf $FZF_CTRL_T_OPTS \
+        --bind 'alt-d:become(cd {} && eval fzf \$FZF_CTRL_T_OPTS)' \
+        --with-nth=-1 --delimiter='/'")"
     zle accept-line
     BUFFER=""
 }
 
 zle -N navigate navigate
-
-bindkey ^@ navigate #ctrl+space / ctrl+@
 
