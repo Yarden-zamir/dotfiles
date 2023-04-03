fc482b181561168ee641540caf6694992501501b
Yarden zamir <make.charcoal@gmail.com>
2023-04-03 20:24:58 +0300

    Move bindings to their own file

--- zshrc/init/completions.zsh
+++ zshrc/init/completions.zsh
@@ -3,6 +3,7 @@ gh_source Aloxaf/fzf-tab/fzf-tab.plugin.zsh
 
 zstyle ':fzf-tab:*' switch-group ',' '.'
 zstyle ':fzf-tab:*' prefix ''
+zstyle ':fzf-tab:*' continuous-trigger 'tab'
 # zstyle ':fzf-tab:*' continuous-trigger 'right' # not needed because of `right:replace-query`
 
 zstyle ':completion:*' fzf-search-display true
@@ -12,10 +13,11 @@ zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS} # set list-colors to enabl
 zstyle ':completion:*:git-checkout:*' sort false # disable sort when completing `git checkout`
 zstyle ':completion:*:descriptions' format '[%d]' # set descriptions format to enable group support
 
-zstyle ':fzf-tab:complete:*' fzf-min-height 20
+zstyle ':fzf-tab:complete:*' fzf-min-height 30
 
-zstyle ':fzf-tab:sources' config-directory "$DOTFILES/zshrc/completion-preview-specs"
-gh_source Freed-Wu/fzf-tab-source/fzf-tab-source.plugin.zsh
+# zstyle ':fzf-tab:sources' config-directory "$DOTFILES/zshrc/completion-preview-specs"
+gh_source yarden-zamir/fzf-tab-source/fzf-tab-source.plugin.zsh
+
 
 
 ZSH_AUTOSUGGEST_STRATEGY=(history completion)
---
+++ zshrc/post-init/bindings.zsh
@@ -0,0 +1,15 @@
+# for global bindings see: $DOTFILES/.skhdrc
+gh_source --require zsh-users/zsh-history-substring-search && {
+    bindkey '^[[A' history-substring-search-up
+    bindkey '^[[B' history-substring-search-down
+}
+
+type -p fzf &>/dev/null && {
+    bindkey '^a' browse_apps
+    bindkey '^@' navigate #ctrl+space / ctrl+@
+    bindkey '^o' fzf-file-or-folder-to-editor
+    bindkey '^z' fzf-cd-widget
+    bindkey '^f' fzf-file-widget
+    type - p rg &>/dev/null && \
+        bindkey '^[ ' ripgrep_search #alt+space
+}
