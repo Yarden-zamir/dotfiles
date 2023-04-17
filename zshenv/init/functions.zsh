346f21bc1712db038f06b6d59cf884b98698f548
Yarden Zamir <hrsidsad@gmail.com>
2023-04-17 12:25:29 +0300

    Add string manipulation functions in python

--- zshenv/init/functions.zsh
+++ zshenv/init/functions.zsh
@@ -18,8 +18,20 @@ function set-shell(){
 function p(){  #executes python code
     prefix_code="import sys
 from sys import stdin
+input = stdin.read().strip()
 "
     python3 -c $prefix_code$1
+}
+function strman(){  # manipulates strings with python
+    prefix_code="import sys
+from sys import stdin
+input = stdin.read().strip()
+input = input. "
+    user_code="$1
+"
+    suffix_code="print(input)"
+    
+    python3 -c $prefix_code$user_code$suffix_code
 }
 
 function calc(){ #calculates in the terminal
