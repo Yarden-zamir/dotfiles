0edd170aa18688d08e644c933c6724b15cc699b1
Yarden Zamir <hrsidsad@gmail.com>
2023-04-17 12:25:47 +0300

    Update ripgrep_search.zsh

--- zshrc/init/ripgrep_search.zsh
+++ zshrc/init/ripgrep_search.zsh
@@ -1,19 +1,19 @@
 # Search for files using names and content glob patterns (unlike the usual aproach of just content)
 ripgrep_search(){
-    RG_PREFIX="rg --ignore-file='$HOME/Library/*' --ignore-file='/Library/*'--column --line-number --no-heading --color=always --dfa-size-limit 2G --smart-case "
+    RG_PREFIX="rg --ignore-file='$HOME/Library/*' --ignore-file='/Library/*'--column --line-number --no-heading --color=always --dfa-size-limit 4G --smart-case "
     RG_PREFIX_filenames="$RG_PREFIX --max-count=1 '' --glob-case-insensitive --glob"
 
     INITIAL_QUERY="$1"
     FZF_DEFAULT_COMMAND="$RG_PREFIX $(printf %q "$INITIAL_QUERY")" \
     fzf --ansi \
         --disabled --query "$INITIAL_QUERY" \
-        --bind "change:reload:sleep 0.1; $RG_PREFIX_filenames '*{q}*' & $RG_PREFIX {q} || true" \
         --delimiter : \
         --preview 'bat --color=always --style=changes,header,header-filename,numbers,rule,snip {1} --highlight-line {2}' \
         --preview-window 'up,60%,border-bottom,+{2}+3/3,~3' \
         --bind 'enter:become(code --goto {1}:{2})' \
         --prompt '1. ripgrep> ' \
         --header 'CTRL-F/R to swich modes' \
+        --bind "change:reload:sleep 0.1; $RG_PREFIX_filenames '*{q}*' & $RG_PREFIX {q} || true" \
         --bind "change:reload:sleep 0.1; $RG_PREFIX {q} || true" \
         --bind "ctrl-r:unbind(change,ctrl-f)+change-prompt(2. fzf> )+enable-search+rebind(ctrl-r)+transform-query(echo {q} > /tmp/rg-fzf-r; cat /tmp/rg-fzf-f)" \
         --bind "ctrl-r:unbind(ctrl-r)+change-prompt(1. ripgrep> )+disable-search+reload($RG_PREFIX {q} || true)+rebind(change,ctrl-f)+transform-query(echo {q} > /tmp/rg-fzf-f; cat /tmp/rg-fzf-r)" \
@@ -21,3 +21,5 @@ ripgrep_search(){
 
 }
 zle -N ripgrep_search ripgrep_search
+#  cat temp.patch | grep '@@ ' | fzf \                                          
+#     --preview 'python3.11 p.py temp.patch "{}" |  bat --color=always --plain --language=Diff' | @ -I{} python3.11 p.py temp.patch "{}"
