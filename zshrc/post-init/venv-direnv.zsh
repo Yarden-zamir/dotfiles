f0aadce7b882d2699de7a26aba295697495a6980
Yarden Zamir <dev@yarden-zamir.com>
2023-04-17 16:33:49 +0300

    Update venv-direnv.zsh

--- zshrc/post-init/venv-direnv.zsh
+++ zshrc/post-init/venv-direnv.zsh
@@ -1,6 +1,6 @@
 type -p direnv &>/dev/null && {
     function venv-direnv() {
-        [[ -f .envrc ]] || echo 'layout python python3' > .envrc
+        [[ -f .envrc ]] || echo '#! /bin/sh \nlayout python python3' > .envrc
         direnv allow
     }
 }
