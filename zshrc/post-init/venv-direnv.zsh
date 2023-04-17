5e1dfe70e80b339715b97e44cd2aa3b9c2aa087d
Yarden Zamir <dev@yarden-zamir.com>
2023-04-17 16:19:00 +0300

    Use official direnv python support

--- zshrc/post-init/venv-direnv.zsh
+++ zshrc/post-init/venv-direnv.zsh
@@ -1,7 +1,6 @@
 type -p direnv &>/dev/null && {
     function venv-direnv() {
-        [[ -d .venv ]] || python3 -m venv .venv
-        [[ -f .envrc ]] || echo 'source .venv/bin/activate' > .envrc
+        [[ -f .envrc ]] || echo 'layout python python3' > .envrc
         direnv allow
     }
 }
