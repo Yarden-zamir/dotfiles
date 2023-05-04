bdcc62398ccd634bed363aa6b5c91378095ce12b
Yarden Zamir <dev@yarden-zamir.com>
2023-05-04 14:00:07 +0300

    zsh-act-completion

--- zshrc/pre-init/compinit.zsh
+++ zshrc/pre-init/compinit.zsh
@@ -2,6 +2,8 @@ export FPATH=$HOME/.zfunc/:$FPATH
 export FPATH=/usr/share/zsh/5.8.1/functions:$FPATH
 export FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
 
+gh_source yarden-zamir/zsh-act-completion \
+    "export FPATH=$FPATH:{}/zsh"
 gh_source perlpunk/shell-completions \
     "export FPATH=$FPATH:{}/zsh"
 
