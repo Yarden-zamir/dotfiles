6db23db068b07da2eb995328699216663286ca9a
Yarden Zamir <dev@yarden-zamir.com>
2023-07-12 16:49:03 +0300

    Update magic-enter.zsh

--- zshrc/init/magic-enter.zsh
+++ zshrc/init/magic-enter.zsh
@@ -1,6 +1,11 @@
 
 function magic-enter-cmd {
-    cmd="clear && exa --icons --all"
+    [ $(ls | wc -l) -gt 15 ] && {
+        cmd=" clear && exa --icons --all" 
+    } || {
+        cmd=" clear && exa --icons --all --long --git --no-permissions --no-user --no-time --group-directories-first"
+    }
+
     if command git rev-parse --is-inside-work-tree &>/dev/null; then
         cmd=$cmd"&& git status -sb | bat --style=plain --language='Git Attributes'"
     fi
