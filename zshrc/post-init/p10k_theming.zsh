c1209e7e64f47b18253d1c3b3b8a92fdec826cc5
Yarden Zamir <dev@yarden-zamir.com>
2024-02-13 10:10:09 +0200

    Update p10k_theming.zsh

--- zshrc/post-init/p10k_theming.zsh
+++ zshrc/post-init/p10k_theming.zsh
@@ -1,4 +1,3 @@
-source /opt/homebrew/opt/powerlevel10k/powerlevel10k.zsh-theme
-
+source /opt/homebrew/opt/powerlevel10k/share/powerlevel10k/powerlevel10k.zsh-theme
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
