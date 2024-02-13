796dfa1cc1d423aa6c23e4f139b700b485a59f8d
Yarden Zamir <dev@yarden-zamir.com>
2024-02-13 10:10:02 +0200

    Create open_with.zsh

---
+++ zshrc/init/open_with.zsh
@@ -0,0 +1,23 @@
+OPEN_WITH_PROGRAMS=(code open github rider github-online)
+open-with(){
+    # open current folder with a program selected with fzf
+    local program
+    program=$(echo -e $OPEN_WITH_PROGRAMS | tr ' ' '\n' | fzf --prompt "Open this folder with")
+    case $program in
+        code)
+            code -r .
+            ;;
+        github-online)
+            gh repo view --web
+            ;;
+        *)
+            $program .
+            ;;
+    esac
+}
+open-with-code(){
+    code -r .
+}
+zle -N open-with open-with
+zle -N open-with-code open-with-code
+
