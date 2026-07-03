949d319707f35df7312109bcb3bb247884d5f64a
Yarden Zamir <dev@yarden-zamir.com>
2026-07-03 20:14:08 +0300

    Update git.zsh

--- zshrc/init/git.zsh
+++ zshrc/init/git.zsh
@@ -13,3 +13,9 @@ git-history() {
    git-fuzzer history
 }
 zle -N git-history git-history
+
+wt() {
+    local target
+    target="$($DOTFILES/bin/wt "$@")" || return
+    [[ -n "$target" ]] && cd "$target"
+}
