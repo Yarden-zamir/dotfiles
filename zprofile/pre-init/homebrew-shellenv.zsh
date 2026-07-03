ba2fe456148025e1626af94df3c5151925980c5d
Yarden Zamir <dev@yarden-zamir.com>
2026-07-03 20:09:55 +0300

    Update homebrew-shellenv.zsh

--- zprofile/pre-init/homebrew-shellenv.zsh
+++ zprofile/pre-init/homebrew-shellenv.zsh
@@ -1,3 +1,11 @@
-if [[ -x /opt/homebrew/bin/brew ]]; then
-  eval "$(/opt/homebrew/bin/brew shellenv)"
+if [[ -d /opt/homebrew ]]; then
+  # Static replacement for `eval "$(brew shellenv)"`; avoids spawning brew on every login shell.
+  export HOMEBREW_PREFIX=/opt/homebrew
+  export HOMEBREW_CELLAR=/opt/homebrew/Cellar
+  export HOMEBREW_REPOSITORY=/opt/homebrew
+  export INFOPATH="/opt/homebrew/share/info:${INFOPATH:-}"
+
+  typeset -gU path fpath
+  path=(/opt/homebrew/bin /opt/homebrew/sbin $path)
+  [[ -d /opt/homebrew/share/zsh/site-functions ]] && fpath=(/opt/homebrew/share/zsh/site-functions $fpath)
 fi
