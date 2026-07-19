289449a99ee5c7ba206f04e2a3f188d8cdd243eb
Yarden Zamir <dev@yarden-zamir.com>
2026-07-19 10:15:33 +0300

    Update navgator.zsh

--- zshrc/init/navgator.zsh
+++ zshrc/init/navgator.zsh
@@ -1,4 +1,7 @@
+# Homebrew-managed; enable exactly one setup path.
+# source "$(brew --prefix navgator)/share/navgator/navgator.zsh"
+
+# Local checkout managed by gh-source.
 gh_source Yarden-zamir/navgator/scripts/navgator.zsh \
-    --skip-build-if-present navgator \
     --skip-build-if-present target/release/navgator \
     --build cargo build --release
